287c4b94d6b7a1796d6b33ec708253c553565b46
Sam <lenry101@gmail.com>
2023-09-02 16:55:25 +0100

    exercise 11

--- src/02-passing-type-arguments/11-data-fetcher.problem.ts
+++ src/02-passing-type-arguments/11-data-fetcher.problem.ts
@@ -1,9 +1,13 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const fetchData = async (url: string) => {
-  const data = await fetch(url).then((response) => response.json());
+// we pass a type to fetchData which is what we expect the return type to be
+// since we return data we need to make sure data is of type T
+const fetchData = async <T>(url: string) => {
+  const data: T = await fetch(url).then((response) => response.json());
   return data;
+  // return data as T;
+  // could also do this
 };
 
 it("Should fetch data from an API", async () => {
