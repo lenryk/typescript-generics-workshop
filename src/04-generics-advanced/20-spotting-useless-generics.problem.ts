5f1afe20a551658393ad6eef239b2695d322009b
Sam <lenry101@gmail.com>
2023-09-07 22:36:11 +0100

    exercise 20

--- src/04-generics-advanced/20-spotting-useless-generics.problem.ts
+++ src/04-generics-advanced/20-spotting-useless-generics.problem.ts
@@ -1,10 +1,10 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnBothOfWhatIPassIn = <T1, T2>(params: {
-  a: T1;
-  b: T2;
-}): [T1, T2] => {
+// we add a constraint to the schema of the generic
+// now we know what to expect we can use object signature to grab the types
+// now we have reduced from 2 generic types to just 1
+const returnBothOfWhatIPassIn = <T1 extends {a: unknown, b: unknown}>(params: T1): [T1["a"], T1["b"]] => {
   return [params.a, params.b];
 };
 
