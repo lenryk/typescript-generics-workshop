19335ac1e9d52b0bb735bee57338cad2d0c4bd83
Sam <lenry101@gmail.com>
2023-09-10 20:59:59 +0100

    exercise 24

--- src/05-function-overloads/24-function-overloads-vs-union-types.problem.ts
+++ src/05-function-overloads/24-function-overloads-vs-union-types.problem.ts
@@ -1,7 +1,13 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-function runGenerator(generator: unknown) {
+
+// we define the two signatures for the object or function version
+// the main implementation has a union for both functions and objects input - and returns string
+// in this case we can just declare the generator as union as the return type doesn't change
+function runGenerator(generator: { run: () => string }): string
+function runGenerator(generator: () => string): string
+function runGenerator(generator: { run: () => string } | (() => string)): string {
   if (typeof generator === "function") {
     return generator();
   }
