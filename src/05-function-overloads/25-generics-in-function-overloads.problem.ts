c4920a50ddb1e235104ea37fcdf706d4e594e374
Sam <lenry101@gmail.com>
2023-09-10 21:45:02 +0100

    exercise 25

--- src/05-function-overloads/25-generics-in-function-overloads.problem.ts
+++ src/05-function-overloads/25-generics-in-function-overloads.problem.ts
@@ -1,7 +1,11 @@
 import { it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-function returnWhatIPassInExceptFor1(t: unknown): unknown {
+// if we pass 1 we want to return a type of 2
+// if we pass a string we want to return the type of whatever the string is
+function returnWhatIPassInExceptFor1(t: 1): 2;
+function returnWhatIPassInExceptFor1<T extends string>(t: T): T;
+function returnWhatIPassInExceptFor1<T>(t: 1 | T): 2 | T {
   if (t === 1) {
     return 2;
   }
